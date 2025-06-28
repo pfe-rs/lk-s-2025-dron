ba6cc6a61d84b8122d6217dcabe7f7bef22d952e
Nikola Najic <nnajic1@gmail.com>
2025-06-28 15:43:00 +0200

    Овај код треба да се дебагује. Код треба да преко блутута пошаље информације за јачине мотора

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,18 @@
 #include <ESP32Servo.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 
 #define RGB_BUILTIN 21
+
+BLEServer *pServer = NULL;
+BLECharacteristic *pTxCharacteristic;
+BLECharacteristic *pRxCharacteristic;
+
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_RX "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
 
 Servo motori[4];
 const int pinoviMotori[] = {7, 8, 9, 10};
@@ -8,7 +20,7 @@ int vrednosti[4];
 String input;
 
 void setup() {
-  rgbLedWrite(RGB_BUILTIN, 10, 0, 0);
+  rgbLedWrite(RGB_BUILTIN, 15, 0, 0);
   Serial.begin(115200);
   
   ESP32PWM::allocateTimer(0);
@@ -17,21 +29,40 @@ void setup() {
   ESP32PWM::allocateTimer(3);
 
   InicijalizacijaMotora();
+  
+  BLEDevice::init("НАЈЈАЧИ ДРОН");
+  pServer = BLEDevice::createServer();
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pTxCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_TX, BLECharacteristic::PROPERTY_NOTIFY);
+
+  pTxCharacteristic->addDescriptor(new BLE2902());
+
+  pRxCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_RX, BLECharacteristic::PROPERTY_WRITE);
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
 }
 
 void loop() {
-  if (Serial.available()) {
-    input = Serial.readStringUntil('\n');
+  String input = pRxCharacteristic->getValue();
+  pRxCharacteristic->setValue("");
+  Serial.println(input);
 
-    int prosli_zarez = -1;
+  int prosli_zarez = -1;
 
-    for (int i = 0; i < 3; i++){
-      int zarez = input.indexOf(',', prosli_zarez + 1);
-      vrednosti[i] = map(input.substring(prosli_zarez + 1, zarez).toInt(), 0, 100, 0, 180);
-      prosli_zarez = zarez;
-    }
-    vrednosti[3] = map(input.substring(prosli_zarez + 1).toInt(), 0, 100, 0, 180);
+  for (int i = 0; i < 3; i++){
+    int zarez = input.indexOf(',', prosli_zarez + 1);
+    vrednosti[i] = map(input.substring(prosli_zarez + 1, zarez).toInt(), 0, 100, 0, 180);
+    prosli_zarez = zarez;
   }
+  vrednosti[3] = map(input.substring(prosli_zarez + 1).toInt(), 0, 100, 0, 180);
   
   PostaviVrednostiMotora();
 }
